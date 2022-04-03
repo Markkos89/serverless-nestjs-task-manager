ce71ef5cbcb1c2f80d195d75678c537357b45e45
Marcos Iglesias <Markkos89@gmail.com>
2022-04-03 08:56:08 -0300

    swagger for dev

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
 
   const app = await NestFactory.create(AppModule);
 
+  // swagger / open api
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager Serverless API')
+    .setDescription(
+      'Documentation with the endpoints we have available for the frontend',
+    )
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // cors
   app.enableCors();
 
   // tells nest to execute a validation pipe whenever he encounters a validation decorator
@@ -21,6 +35,7 @@ async function bootstrap() {
   await app.listen(PORT);
 
   logger.log(`Application listening on port ${PORT}`);
+  logger.log(`Swagger available on: http://localhost:8080/docs/`);
 }
 
 bootstrap();
